936431eb2c0b2502af860ae40f928067c1bc30cc
david cromberge <david.cromberge@pure360.com>
2014-10-24 15:13:46 +0100

    functional data structures exercises

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -46,20 +46,52 @@ object List { // `List` companion object. Contains functions for creating and wo
   def product2(ns: List[Double]) = 
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  def tail[A](l: List[A]): List[A] = 
+    l match {
+      case Cons(_, t)  => t
+      case Nil => Nil
+    }
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = 
+    l match {
+      case Cons(_, t) => Cons(h, t)
+      case Nil => Nil
+    }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def drop[A](l: List[A], n: Int): List[A] = 
+    if (n > 0) 
+      drop(tail(l), n-1)
+    else l
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = 
+    l match {
+      case Cons(h, t) if f(h) => dropWhile(t, f)
+      case _ => l
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  def init[A](l: List[A]): List[A] = 
+    l match {
+      case Cons(h, Nil) => Nil
+      case Cons(h, t) => Cons(h, init(t))
+      case Nil => Nil
+    }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  def length[A](l: List[A]): Int = 
+    l match {
+      case Cons(h, t) => 1 + length(t)
+      case _ => 0
+    }
+    
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = 
+    l match {
+      case Nil => z
+      case Cons(h, t) => f(foldLeft(t, z)(f), h)
+    }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
-
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  def map[A,B](l: List[A])(f: A => B): List[B] = 
+    l match {
+      case Nil => Nil
+      case Cons(h, t) => Cons(f(h), map(t)(f))
+    }
 }
