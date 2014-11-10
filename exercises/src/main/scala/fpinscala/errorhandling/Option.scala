cbfd0ba4d6cab355b0ce4e6a7f19085ffdfcb983
davecromberge <davecromberge@gmail.com>
2014-11-10 23:46:08 +0000

    work in progress on Option ADT

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -67,7 +67,14 @@ object Option {
     } yield f(a1, b1)
 
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = {
+    val seed: Option[List[A]] = Some(Nil)
+    a.foldRight(seed)((a, b) => a.flatMap(h => b.map(t => h :: t)))
+  }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    val seed: Option[List[B]] = Some(Nil)
+    a.foldRight(seed)((a, b) => f(a).flatMap(h => b.map(t => h :: t)))
+  }
+
 }
