fe8d3e6a08a73f1293e75c3c35a2e39ce3c4c758
davecromberge <davecromberge@gmail.com>
2014-12-23 20:35:51 +0000

    implement choiceMap on Nonblocking Par

--- exercises/src/main/scala/fpinscala/parallelism/Nonblocking.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Nonblocking.scala
@@ -140,7 +140,10 @@ object Nonblocking {
       })(List(ifFalse, ifTrue))
 
     def choiceMap[K,V](p: Par[K])(ps: Map[K,Par[V]]): Par[V] =
-      ???
+      es => new Future[V] {
+        def apply(cb: V => Unit): Unit =
+          p(es) { k => ps(k)(es)(cb) }
+      }
 
     // see `Nonblocking.scala` answers file. This function is usually called something else!
     def chooser[A,B](p: Par[A])(f: A => Par[B]): Par[B] =
