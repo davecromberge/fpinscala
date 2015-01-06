a3b31e09233546ba4636d846fd7fd3d7157cc128
david cromberge <david.cromberge@pure360.com>
2015-01-06 16:37:08 +0000

    implement weighted on Gen

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -28,7 +28,12 @@ case class Gen[A](sample: State[RNG,A]) {
 
   def map[B](f: A => B): Gen[B] = Gen(sample.map(f))
 
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+  def flatMap[B](f: A => Gen[B]): Gen[B] =
+    Gen(State { rng =>
+      val (a, rng2) = sample.run(rng)
+      val (b, rng3) = f(a).sample.run(rng2)
+      (b, rng3)
+    })
 }
 
 object Gen {
@@ -38,10 +43,25 @@ object Gen {
     Gen(State(rng => RNG.rangedInt(rng)(start, stopExclusive)))
 
   def boolean: Gen[Boolean] =
-    Gen.choose(Int.MinValue, Int.MaxValue).map(x => if (x % 2 == 0) false else true)
+    choose(Int.MinValue, Int.MaxValue).map(x => if (x % 2 == 0) false else true)
+
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    weighted((g1, 0.5), (g2, 0.5))
 
   def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
     Gen(State.sequence(List.fill(n)(g.sample)))
+
+  def weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
+    val (gen1, p1) = g1
+    val (gen2, p2) = g2
+    require(p1 + p2 == 1)
+
+    choose(0, Int.MaxValue).flatMap { x => 
+      val p1v = p1 * Int.MaxValue
+      val p2v = p2 * Int.MaxValue
+      if (x < p1v) gen1 else gen2
+    }
+  }
 }
 
 trait SGen[+A] {
